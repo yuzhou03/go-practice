c618f95a2cab5f65823ea5511c0adf237f177be6
Zhou Yu <yuzhou03@qq.com>
2017-12-16 16:30:35 +0800

    add NowStr()

--- time/sleep.go
+++ time/sleep.go
@@ -1,9 +1,19 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	print("开始...\n")
-	time.Sleep(time.Second * 1)
-	print("结束")
+	//print("开始...\n")
+	//time.Sleep(time.Second * 1)
+	//print("结束")
+
+	fmt.Println(NowStr())
+}
+
+func NowStr() string {
+	timeStr := time.Now().Format("2006-01-02 15:04:05")
+	return timeStr
 }
